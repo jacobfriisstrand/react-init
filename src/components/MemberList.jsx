49b614257aac4c7db6311d581489894843b9fa14
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-10-26 09:46:08 +0200

    image path updated

--- src/components/MemberList.jsx
+++ src/components/MemberList.jsx
@@ -5,9 +5,9 @@ export default function MemberList() {
     <div className="member__container">
       <p className="memberlist__header">Team members previosly added to projects</p>
       <div className="member__list">
-        <User name="John Doe" position="Developer" image="https://source.unsplash.com/random/?avatar" />
-        <User name="Alice Smith" position="Designer" image="https://source.unsplash.com/random/?avatar" />
-        <User name="Bob Johnson" position="Manager" image="https://source.unsplash.com/random/?avatar" />
+        <User name="John Doe" position="Developer" image="https://source.unsplash.com/random/?portrait,male" />
+        <User name="Alice Smith" position="Designer" image="https://source.unsplash.com/random/?portrait,female" />
+        <User name="Bob Johnson" position="Manager" image="https://source.unsplash.com/random/?portrait,unicorn" />
       </div>
     </div>
   );
