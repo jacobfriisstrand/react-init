ac92478013b8070657721fcaa1de2df52dff8a69
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-10-26 09:51:31 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,7 @@ export default function App() {
     <div className="wrapper">
       <header>
         <h1>My first React components</h1>
-        <p>The assigment was to recreate the following UI.</p>
-        <img src="src/assets/team-member-ui.png" alt="UI Screenshot" />
-        <p>The components can be divided into what is going to be reused in this UI, and potentially in other parts of a larger project.</p>
-        <img src="src/assets/component-overview.png" alt="Component overview" />
+        <p>The assigment was to recreate a UI from the Tailwind website.</p>
         <h2>Check out my solution below</h2>
       </header>
       <main>
